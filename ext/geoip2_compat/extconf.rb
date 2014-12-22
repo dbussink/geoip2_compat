06164e1f289854d7562ff65fb14d45c1b30f309e
Joshua Peek <josh@joshpeek.com>
2014-12-22 11:52:15 -0800

    Compile libmaxminddb

--- ext/geoip2_compat/extconf.rb
+++ ext/geoip2_compat/extconf.rb
@@ -6,6 +6,16 @@ $CFLAGS << " #{ENV['CFLAGS']}"
 
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
+ROOT = File.expand_path("../../..", __FILE__)
+
+Dir.chdir("#{ROOT}/vendor/libmaxminddb-0.5.3") do
+  system "./configure", "--prefix", "#{ROOT}/ext/libmaxminddb"
+  system "make", "install"
+end
+
+$INCFLAGS << " -I#{ROOT}/ext/libmaxminddb/include "
+$LDFLAGS  << " -L#{ROOT}/ext/libmaxminddb/lib "
+
 if have_header('maxminddb.h') && have_library('maxminddb')
   create_makefile("#{extension_name}/#{extension_name}")
 else
